5c6c2ab510dc601ed8975df18c251711f00399ee
stevenjose <lopezajoseg@gmail.com>
2023-04-07 01:59:21 +0200

    report coverage 80%

--- karma.conf.js
+++ karma.conf.js
@@ -34,10 +34,10 @@ module.exports = function (config) {
       ],
       check: {
         global: {
-          statements: 30,
-          branches: 30,
-          funtions: 30,
-          lines: 30
+          statements: 80,
+          branches: 80,
+          funtions: 80,
+          lines: 80
         }
       }
     },
